f696d61d57a4ab4650927bc5843ae28271760669
María José de Juan <rodi_no_miko@yahoo.com>
2023-01-30 13:54:33 +0100

    CMS_01

---
+++ helper/DBHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Helper;
+
+class DBHelper
+{
+
+    var $db;
+
+    function __construct(){
+
+        //Conexión mediante PDO
+        $opciones = [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"];
+        try {
+            $this->db = new \PDO(
+                'mysql:host=localhost;dbname=cms',
+                'usuario-cms',
+                'introduce-tu-contrasena',
+                $opciones);
+            $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        } catch (\PDOException $e) {
+            echo 'Falló la conexión: ' . $e->getMessage();
+        }
+
+    }
+
+}
